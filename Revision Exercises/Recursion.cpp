4771977a2e59d1a169e2cdb4efff651df6e586b4
Derek Flood <derek.flood@dkit.ie>
2022-09-20 09:46:38 +0100

    Question 1 complete, Question 2 function definitions included

--- Revision Exercises/Recursion.cpp
+++ Revision Exercises/Recursion.cpp
@@ -2,11 +2,23 @@
 
 bool isPalindrome(std::string s)
 {
-
+	if (s.size() < 2)
+		return true;
+	if (s[0] != s[s.size() - 1])
+		return false;
+	else
+		return isPalindrome
+		(s.substr(1, s.size() - 2));
 }
 void recursionQuestion1Test()
 {
-
+	string* arr = new string[5]{"racecar", "navan",
+	"sheldon", "test", "teet"};
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr[i] << ": " << 
+			(isPalindrome(arr[i])?"yes":"no")<<endl;
+	}
 }
 
 
--- Revision Exercises/Recursion.h
+++ Revision Exercises/Recursion.h
@@ -2,5 +2,7 @@
 #include <iostream>
 using namespace std;
 void recursionQuestion1Test();
+void recursionQuestion2Test();
 
 bool isPalindrome(string s);
+void findMinMax(int* arr, int size, int& min, int& max, int pos);
